4b79c710fc677eeeb2e59465aa3eb6a3d46342a4
cybergausho <lauttmz@gmail.com>
2025-10-06 20:03:46 -0300

    pase arreglo de superhero a listas

--- parte2_materia/Program.cs
+++ parte2_materia/Program.cs
@@ -4,21 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //pasamos a listas
+            List<Superheroe> ligaJusticiaMultiverso= new List<Superheroe>();
 
-            //arreglo de 2 superheroes
-            Superheroe[] ligaJusticia = new Superheroe[2];
 
             //armas - clase aparte -- independi de un superheroe 
             Arma arcoFlecha = new Arma("Arco y flecha", 10);
             Arma boleadora = new Arma("Boleadora", 1000);
             Arma espada = new Arma("Espada tallada por elfos", 1000);
             Arma Revolver = new Arma("Revolver ", 10);
+            Arma batiprofeno = new Arma("BatiProfeno", 100);
 
-            ligaJusticia[0] = new Superheroe("Patoruzito", "Captura", boleadora);
-            ligaJusticia[1] = new Superheroe("Legolass", "Flechazo mistico", arcoFlecha);
+            Superheroe patoruzito = new Superheroe("Patoruzito", "Captura", boleadora);
 
-            foreach(Superheroe sh in ligaJusticia)
+            ligaJusticiaMultiverso.Add(patoruzito);
+            ligaJusticiaMultiverso.Add(new Superheroe("Legolass", "Flechazo mistico", arcoFlecha));
+            ligaJusticiaMultiverso.Add(new Superheroe("Bataman", "batisanar", batiprofeno));
+
+            //eliminar elementos de una lista por indice
+            ligaJusticiaMultiverso.RemoveAt(2);
+
+            // eliminar elementos de una lista por objetos
+            //ligaJusticiaMultiverso.Remove(patoruzito);
+            ligaJusticiaMultiverso.RemoveAll();
+
+            foreach (Superheroe sh in ligaJusticiaMultiverso)
             {
                 sh.presentarse();
             }
